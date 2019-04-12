a118708e783ff2e4023044cc0ac6319e103f1293
Xon <tazipper1@hotmail.com>
2019-04-12 11:58:54 +0800

    Migrate XF1 options into a more sane format

--- upload/src/addons/SV/DailyStatistics/Setup.php
+++ upload/src/addons/SV/DailyStatistics/Setup.php
@@ -28,6 +28,105 @@ class Setup extends AbstractSetup
             'MWSDailyStats', 'MWSviewStats',
             'general', 'svViewExtraStats'
         );
+
+        $extend = [];
+        if ($this->hasOptionSet('dailystats_fh_users'))
+        {
+            $extend[] = 'latestUsers';
+        }
+        if ($this->hasOptionSet('dailystats_fh_discussions'))
+        {
+            $extend[] = 'threads';
+        }
+        if ($this->hasOptionSet('dailystats_fm_messages'))
+        {
+            $extend[] = 'posts';
+        }
+        if ($this->hasOptionSet('dailystats_fh_resource'))
+        {
+            $extend[] = 'resources';
+        }
+        if ($this->hasOptionSet('dailystats_fh_xfmg'))
+        {
+            $extend[] = 'mediaItems';
+        }
+
+        $this->renameOption('dailystats_acp_extended','svDailyStatistics_extendedStatsInDashboard');
+        /** @var \XF\Entity\Option $option */
+        $option = \XF::finder('XF:Option')
+                     ->whereId('svDailyStatistics_publicWidgetStatistics')
+                     ->fetchOne();
+        if (!$option)
+        {
+            $option = \XF::em()->create('XF:Option');
+            $option->option_id = 'svDailyStatistics_publicWidgetStatistics';
+            $option->setOption('verify_validation_callback', false);
+            $option->setOption('verify_value', false);
+            $option->addon_id = $this->addOn->getAddOnId();
+            $option->edit_format = 'template';
+            $option->edit_format_params = 'option_template_svDailyStatistics_dashboardStatistics';
+            $option->data_type = 'array';
+            $option->sub_options = ['*'];
+        }
+        $option->option_value = $extend;
+        $option->saveIfChanged();
+
+        // ACP extend option
+        $extend = [];
+        if ($this->hasOptionSet('dailystats_acp_users'))
+        {
+            $extend[] = 'activeUsers';
+            $extend[] = 'latestUsers';
+        }
+        if ($this->hasOptionSet('dailystats_acp_discussions'))
+        {
+            $extend[] = 'threads';
+        }
+        if ($this->hasOptionSet('dailystats_acp_messages'))
+        {
+            $extend[] = 'posts';
+        }
+        if ($this->hasOptionSet('dailystats_acp_resource'))
+        {
+            $extend[] = 'resources';
+        }
+        if ($this->hasOptionSet('dailystats_acp_xfmg'))
+        {
+            $extend[] = 'mediaItems';
+        }
+
+        $this->renameOption('dailystats_forum_home','svDailyStatistics_showInForumStatisticsWidget');
+        /** @var \XF\Entity\Option $option */
+        $option = \XF::finder('XF:Option')
+                     ->whereId('svDailyStatistics_dashboardStatistics')
+                     ->fetchOne();
+        if (!$option)
+        {
+            $option = \XF::em()->create('XF:Option');
+            $option->option_id = 'svDailyStatistics_dashboardStatistics';
+            $option->setOption('verify_validation_callback', false);
+            $option->setOption('verify_value', false);
+            $option->addon_id = $this->addOn->getAddOnId();
+            $option->edit_format = 'template';
+            $option->edit_format_params = 'option_template_svDailyStatistics_dashboardStatistics';
+            $option->data_type = 'array';
+            $option->sub_options = ['*'];
+        }
+        $option->option_value = $extend;
+        $option->saveIfChanged();
+    }
+
+    protected function hasOptionSet($optionName)
+    {
+        $option = \XF::finder('XF:Option')
+                     ->whereId($optionName)
+                     ->fetchOne();
+        if ($option)
+        {
+            return (bool)$option->option_value;
+        }
+
+        return false;
     }
 
     /**
